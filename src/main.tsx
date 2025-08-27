22b56209edf2aeb428e05ab4d5bebf5aabf82414
franmillaauria <fran.milla@innovaexport.com>
2025-08-27 13:13:06 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'   // 👈
+import { HashRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
