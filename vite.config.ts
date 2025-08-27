4b95d17cb8be311330eed89dc31068678b1d4910
franmillaauria <fran.milla@innovaexport.com>
2025-08-27 12:53:32 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({ plugins:[react()], base:'/arcos-chat/' })  plugins: [react()],
+export default defineConfig({
+  plugins: [react()],
+  // IMPORTANTE: el nombre del repo, con barra inicial y final
+  base: '/arcos-chat/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  // (opcional; por defecto ya es 'dist')
+  build: {
+    outDir: 'dist',
   },
 })
